21ab1c42fbc36ec86b179f12fbb1089cfc3b2adf
unknown <LPT-1944@LPT1944.newgen.co.in>
2022-01-22 21:09:34 +0530

    mocked cars data added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import cars from "./data/cars";
 
 function App() {
+  console.log(cars);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {cars.map((el, index) => {
+        return <img style={{height:'300px',width:'550px'}}key={index} src={el.imagePath}></img>;
+      })}
     </div>
   );
 }
